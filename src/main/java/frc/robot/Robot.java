90c1a75496f3357be4239cf9a52352d8a2822c18
SkywardSyntax <87048477+SkywardSyntax@users.noreply.github.com>
2023-11-29 05:31:46 +0000

    Hopefully stopped robot drift

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.swerve.DriveSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -18,6 +19,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+  private DriveSubsystem m_robotDrive;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -28,6 +30,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    m_robotDrive = new DriveSubsystem();
   }
 
   /**
@@ -56,7 +59,10 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
+
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    m_robotDrive.resetGyro();
+    m_robotDrive.recalibrateGyro();
     
     /*
      * String autoSelected = SmartDashboard.getString("Auto Selector",
--- src/main/java/frc/robot/subsystems/swerve/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swerve/DriveSubsystem.java
@@ -257,4 +257,8 @@ public class DriveSubsystem extends SubsystemBase {
   public double getTurnRate() {
     return m_gyro.getRate() * (DriveConstants.kGyroReversed ? -1.0 : 1.0);
   }
+
+  public void recalibrateGyro() {
+    m_gyro.calibrate();
+  }
 }
