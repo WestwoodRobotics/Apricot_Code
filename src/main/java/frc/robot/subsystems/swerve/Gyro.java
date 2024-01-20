131c2947b094de7cde5e11d4abe533d44e54156a
tangerine tnt <afjlaskdfjalsdkfjaskdlfjasd@asd.com>
2024-01-19 18:35:45 -0600

    Modified Gyro

--- src/main/java/frc/robot/subsystems/swerve/Gyro.java
+++ src/main/java/frc/robot/subsystems/swerve/Gyro.java
@@ -25,23 +25,12 @@ public class Gyro extends SubsystemBase {
   public Rotation2d rollOffset = new Rotation2d(0);
 
   /**
-   * Creates a new Gyro, which is a wrapper for the Pigeon IMU and stores an offset so we don't
+   * Creates a new Gyro, which is a wrapper for the ADIS16740 IMU and stores an offset so we don't
    * have to directly zero the gyro
    */
   public Gyro() {
     gyro = new ADIS16470_IMU();
     yawAxis = gyro.getYawAxis();
-
-    gyro.setYawAxis(pitchAxis);
-    pitchAxis = gyro.getYawAxis();
-
-    gyro.setYawAxis(rollAxis);
-    rollAxis = gyro.getYawAxis();
-
-    gyro.setYawAxis(yawAxis);
-
-    yawAxis = gyro.getYawAxis();
-    
     zeroGyro();
   }
 
@@ -80,90 +69,12 @@ public class Gyro extends SubsystemBase {
 
 
 
-  public void setGyroPitchOffset(double degrees) {
-    gyro.setYawAxis(pitchAxis);
-    pitchOffset = getRawRot2dPitch().minus(Rotation2d.fromDegrees(degrees));
-    gyro.setYawAxis(yawAxis);
-  }
-
-  public Rotation2d getProcessedRot2dPitch() {
-    return getRawRot2dPitch().minus(pitchOffset).times(-1);
-  }
-
-  public Rotation2d getRawRot2dPitch() {
-    gyro.setYawAxis(pitchAxis);
-    double angle = gyro.getAngle();
-    gyro.setYawAxis(yawAxis);
-    return Rotation2d.fromDegrees(angle);
-  }
-
-  public double getRawPitch(){
-    gyro.setYawAxis(pitchAxis);
-    double angle = gyro.getAngle();
-    gyro.setYawAxis(yawAxis);
-    return angle;
-  }
-
-  public void resetPitch(){
-    gyro.setYawAxis(pitchAxis);
-    gyro.reset();
-    gyro.setYawAxis(yawAxis);
-  }
-
-  public double getPitchRate(){
-    gyro.setYawAxis(pitchAxis);
-    double rate = gyro.getRate();
-    gyro.setYawAxis(yawAxis);
-    return rate;
-  }
-
-
-
-
-
-  public void setGyroRollOffset(double degrees) {
-    gyro.setYawAxis(rollAxis);
-    rollOffset = getRawRot2dRoll().minus(Rotation2d.fromDegrees(degrees));
-    gyro.setYawAxis(yawAxis);
-  }
-
-  public Rotation2d getProcessedRot2dRoll() {
-    return getRawRot2dRoll().minus(rollOffset).times(-1);
-  }
-
-  public Rotation2d getRawRot2dRoll() {
-    gyro.setYawAxis(rollAxis);
-    double angle = gyro.getAngle();
-    gyro.setYawAxis(yawAxis);
-    return Rotation2d.fromDegrees(angle);
-  }
-
-  public double getRawRoll(){
-    gyro.setYawAxis(rollAxis);
-    double angle = gyro.getAngle();
-    gyro.setYawAxis(yawAxis);
-    return angle;
-  }
-
-  public void resetRoll(){
-    gyro.setYawAxis(rollAxis);
-    gyro.reset();
-    gyro.setYawAxis(yawAxis);
-  }
-
-  public double getRollRate(){
-    gyro.setYawAxis(rollAxis);
-    double rate = gyro.getRate();
-    gyro.setYawAxis(yawAxis);
-    return rate;
-  }
+ 
 
 
 
 
   public void reset(){
-    this.resetRoll();
-    this.resetPitch();
     this.resetYaw();
   }
     
